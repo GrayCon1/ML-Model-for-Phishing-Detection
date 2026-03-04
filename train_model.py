be8f0a74815f94b99f2a34c8630422b3abb98965
GrayCon1 <129750528+GrayCon1@users.noreply.github.com>
2026-03-04 15:59:38 +0200

    fixing class weighting with different thresholds

--- train_model.py
+++ train_model.py
@@ -1,16 +1,49 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix
 
 # Load the dataset
 df = pd.read_csv("phishing_email_dataset.csv")
 
-print("First 5 rows:\n")
-print(df.head())
+# Spearate features and lables
+X = df.drop("label", axis=1)
+y = df["label"]
 
-print("\nColumn names:\n")
-print(df.columns)
+# Split dataset
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+ )
 
-print("\nDataset shape:")
-print(df.shape)
+print("Training samples:", len(X_train))
+print("Testing samples:", len(X_test))
 
-print("\nMissing values in each column:")
-print(df.isnull().sum())
+# Train model
+model = LogisticRegression(max_iter=1000, class_weight="balanced")
+model.fit(X_train, y_train)
+
+print("Model training completed.")
+
+# Predict
+predictions =model.predict(X_test)
+
+# Evaluate
+print("\nClassification Report:\n")
+print(classification_report(y_test, predictions))
+
+print("\nConfusion Matrix:\n")
+print(confusion_matrix(y_test, predictions))
+
+probs = model.predict_proba(X_test)[:,1]
+
+import numpy as np
+
+# Try different thresholds
+threshold = 0.4 # Adjust this threshold based on your needs
+
+custom_predictions = (probs > threshold).astype(int)
+
+print("\nCustom Threshold:", threshold)
+print(classification_report(y_test, custom_predictions))
+print("\nConfusionm Matrix:")
+print(confusion_matrix(y_test, custom_predictions))
